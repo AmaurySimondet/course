ebd56a02008c7a700c47eedcd7b34ca2564531cd
Amaury Simondet <amaury.simondet@hotmail.com>
2026-02-18 15:22:27 +0100

    50%

--- airbnb/models/fct/fct_reviews.sql
+++ airbnb/models/fct/fct_reviews.sql
@@ -10,6 +10,8 @@ WITH src_reviews AS (
         {{ ref('src_reviews') }}
 )
 SELECT
+    {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }}
+    AS review_id,
     *
 FROM
     src_reviews
